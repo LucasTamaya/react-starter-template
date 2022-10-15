1d7190b7e0557d87b606ec9715a02978b9cff2ae
LucasTamaya <lucastamaya.974@gmail.com>
2022-10-15 06:57:48 +0400

    Update to enable react-async-helmet
    - add the provider in the index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./style/index.css";
+import { HelmetProvider } from "react-helmet-async";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+const helmetContext = {};
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider context={helmetContext}>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
